0e7a2ed6448db47440682e0814163dc528d364b4
Eduardo0725 <eduardom0142@gmail.com>
2020-06-29 01:07:33 -0300

    ReactJS com TypeScript

---
+++ Javascript/reactjs_com_typescript/contacts-app/src/contacts.ts
@@ -0,0 +1,46 @@
+const CONTACTS = [
+    {
+        id: "dec55746-2a07-4ab5-b8db-7c756e2a0833",
+        name: "Lorrin Wingham",
+        email: "lwingham0@yandex.ru",
+        phoneNumber: "1894673470"
+    },
+    {
+        id: "dec55746-2a07-4ab5-b8db-7c756e2a0834",
+        name: "Lonnie Greaser",
+        email: "lgreaser0@yandex.ru",
+        phoneNumber: "9944151058"
+    },
+    {
+        id: "dec55746-2a07-4ab5-b8db-7c756e2a0835",
+        name: "Matthus Schaumaker",
+        email: "mschaumaker@yandex.ru",
+        phoneNumber: "3864519591"
+    },
+    {
+        id: "dec55746-2a07-4ab5-b8db-7c756e2a0836",
+        name: "Matthus Schaumaker",
+        email: "mschaumaker@yandex.ru",
+        phoneNumber: "3864519591"
+    },
+    {
+        id: "dec55746-2a07-4ab5-b8db-7c756e2a0837",
+        name: "Matthus Schaumaker",
+        email: "mschaumaker@yandex.ru",
+        phoneNumber: "3864519591"
+    },
+    {
+        id: "dec55746-2a07-4ab5-b8db-7c756e2a0838",
+        name: "Matthus Schaumaker",
+        email: "mschaumaker@yandex.ru",
+        phoneNumber: "3864519591"
+    },
+    {
+        id: "dec55746-2a07-4ab5-b8db-7c756e2a0839",
+        name: "Matthus Schaumaker",
+        email: "mschaumaker@yandex.ru",
+        phoneNumber: "3864519591"
+    }
+];
+
+export default CONTACTS;
